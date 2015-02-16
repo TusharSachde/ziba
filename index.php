fed5117d7e0d265d1369d283ff42f9c4fc095d62
sohan1 <shn619@gmail.com>
2015-02-16 14:00:40 +0530

    , mlk lk.

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
     <header>
         <div class="top-bar barheight">
             <div class="row">
-                <div class="col-xs-3"><span class="bold spacer">WELCOME TO ZIBA</span>
+                <div class="col-xs-3"><span class="bold spacer">Join the IHEARTZIBA Club</span>
                 </div>
                 <div class="col-xs-6 text-center"><span class="bold spacer">{{msgg}}</span>
                 </div>
